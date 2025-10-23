e8b366c58ab35ca69478abfd250d329039622a0e
Stephen Sviatko <ssviatko@gmail.com>
2025-10-22 20:42:40 -0700

    Add urandom support

--- dh.c
+++ dh.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <gmp.h>
 #include <time.h>
 #include <getopt.h>
@@ -13,21 +17,32 @@ unsigned char l_b[MAXBYTEBUFF];
 
 struct option g_options[] = {
 	{ "bits", required_argument, NULL, 'b' },
+	{ "help", no_argument, NULL, '?' },
 	{ NULL, 0, NULL, 0 }
 };
 
 int main(int argc, char **argv)
 {
 	unsigned int i;
-	unsigned int l_bits = 512;
+	int l_urandom_fd; // file descriptor for /dev/urandom
+	int res; // result variable for UNIX reads
+	unsigned int l_bits = 512; // default bit width
 	int opt;
-	srand(time(NULL));
-	while ((opt = getopt_long(argc, argv, "b:", g_options, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "b:?", g_options, NULL)) != -1) {
 		switch (opt) {
 			case 'b':
 				{
 					l_bits = atoi(optarg);
 				}
+				break;
+			case '?':
+				{
+					printf("usage: dh -b (--bits) <bit width>\n");
+					printf("  must be between 128-8192 in 64 bit increments\n");
+					printf("  default: 512 bits\n");
+					exit(EXIT_SUCCESS);
+				}
+				break;
 		}
 	}
 	if (l_bits > 8192) {
@@ -49,10 +64,20 @@ int main(int argc, char **argv)
 	printf("DH bit width: %d\n", l_bits);
 	printf("Private bit width: %d\n", l_privbits);
 
+	// open urandom
+	l_urandom_fd = open("/dev/urandom", O_RDONLY);
+	if (l_urandom_fd < 0) {
+		fprintf(stderr, "dh: problems opening /dev/urandom: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Preparing %d-bit p value...\n", l_bits);
+
 	// prepare random n-bit odd number for DH p factor
-	for (i = 0; i < (l_bits / 8); ++i) {
-		l_p[i] = rand() % 256;
+	res = read(l_urandom_fd, l_p, (l_bits / 8));
+	if (res != (l_bits / 8)) {
+		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 	l_p[0] |= 0x80; // make it between 2^n - 1 and 2^(n-1)
 	l_p[(l_bits / 8) - 1] |= 0x01; // make it odd
@@ -72,8 +97,10 @@ int main(int argc, char **argv)
 	printf("mpz_probab_prime_p now returns %d.\n", l_pp);
 
 	printf("Preparing %d-bit a value...\n", l_privbits);
-	for (i = 0; i < (l_privbits / 8); ++i) {
-		l_a[i] = rand() % 256;
+	res = read(l_urandom_fd, l_a, (l_privbits / 8));
+	if (res != (l_privbits / 8)) {
+		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 	mpz_t l_a_import;
 	mpz_init(l_a_import);
@@ -81,8 +108,10 @@ int main(int argc, char **argv)
 	gmp_printf("a = %Zx\n", l_a_import);
 
 	printf("Preparing %d-bit b value...\n", l_privbits);
-	for (i = 0; i < (l_privbits / 8); ++i) {
-		l_b[i] = rand() % 256;
+	res = read(l_urandom_fd, l_b, (l_privbits / 8));
+	if (res != (l_privbits / 8)) {
+		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 	mpz_t l_b_import;
 	mpz_init(l_b_import);
@@ -92,7 +121,12 @@ int main(int argc, char **argv)
 	printf("Preparing g value...\n");
 	mpz_t l_g;
 	mpz_init(l_g);
-	unsigned int l_g_rand = rand() % 16;
+	unsigned int l_g_rand;
+	res = read(l_urandom_fd, &l_g_rand, sizeof(l_g_rand));
+	if (res != sizeof(l_g_rand)) {
+		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 	// l_g_rand even/odd?
 	if ((l_g_rand & 0x01) == 0)
 		mpz_set_ui(l_g, 3);
@@ -100,7 +134,11 @@ int main(int argc, char **argv)
 		mpz_set_ui(l_g, 5);
 	gmp_printf("g = %Zd\n", l_g);
 
-	/* at this point in the DH exchange, Alice would generate p and g and send them to Bob,
+	// done with random values, so close urandom_fd
+	close(l_urandom_fd);
+
+	/* at this point in the DH exchange, Alice would have already generated p and g
+	 * (and send them to Bob,)
 	 * so that both Alice and Bob have the same p and g. Then each Alice and Bob would
 	 * generate a private value (a for Alice, b for Bob) and keep these secret. Then
 	 * Alice computes A = g^a mod p, Bob computes B = g^b mod p, and Alice and Bob
