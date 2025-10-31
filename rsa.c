6a76b16e4de3cd3dad696aa04cfb101fa339dd16
Stephen Sviatko <ssviatko@gmail.com>
2025-10-30 18:42:20 -0700

    got rudimentary RSA algorithm working

--- rsa.c
+++ rsa.c
@@ -8,11 +8,12 @@
 #include <time.h>
 #include <getopt.h>
 
-#define MAXBITS 8192
+#define MAXBITS 16384
 #define MAXBYTEBUFF (MAXBITS / 8)
 
 unsigned char l_p[MAXBYTEBUFF];
 unsigned char l_q[MAXBYTEBUFF];
+unsigned char l_pt[MAXBYTEBUFF];
 
 struct option g_options[] = {
 	{ "bits", required_argument, NULL, 'b' },
@@ -25,7 +26,7 @@ int main(int argc, char **argv)
 	unsigned int i;
 	int l_urandom_fd; // file descriptor for /dev/urandom
 	int res; // result variable for UNIX reads
-	unsigned int l_bits = 256; // default bit width
+	unsigned int l_bits = 128; // default bit width
 	int opt;
 	while ((opt = getopt_long(argc, argv, "b:?", g_options, NULL)) != -1) {
 		switch (opt) {
@@ -57,11 +58,8 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	int l_privbits = (l_bits / 8) + 96;
-
 	printf("RSA toolbox\n");
 	printf("RSA p/q bit width: %d\n", l_bits);
-	printf("Private bit width: %d\n", l_privbits);
 
 	// open urandom
 	l_urandom_fd = open("/dev/urandom", O_RDONLY);
@@ -90,7 +88,7 @@ int main(int argc, char **argv)
 //		printf("calling mpz_nextprime...\n");
 		mpz_nextprime(l_p_import, l_p_import);
 	}
-	gmp_printf("p = %Zx\n", l_p_import);
+	gmp_printf("p       = %Zx\n", l_p_import);
 	l_pp = mpz_probab_prime_p(l_p_import, 50);
 //	printf("mpz_probab_prime_p now returns %d.\n", l_pp);
 
@@ -114,7 +112,7 @@ int main(int argc, char **argv)
 //		printf("calling mpz_nextprime...\n");
 		mpz_nextprime(l_q_import, l_q_import);
 	}
-	gmp_printf("q = %Zx\n", l_q_import);
+	gmp_printf("q       = %Zx\n", l_q_import);
 	l_pp = mpz_probab_prime_p(l_q_import, 50);
 //	printf("mpz_probab_prime_p now returns %d.\n", l_pp);
 
@@ -132,8 +130,73 @@ int main(int argc, char **argv)
 	mpz_t l_n;
 	mpz_init(l_n);
 	mpz_mul(l_n, l_p_import, l_q_import);
-	gmp_printf("n = %Zx\n", l_n);
+	gmp_printf("n       = %Zx\n", l_n);
 
+	// prepare phi = p1 * q1
+//	mpz_t l_phi;
+//	mpz_init(l_phi);
+//	mpz_mul(l_phi, l_p1, l_q1);
+//	gmp_printf("phi     = %Zx\n", l_phi);
+
+	// prepare carmichael totient
+	mpz_t l_ct;
+	mpz_init(l_ct);
+	mpz_lcm(l_ct, l_p1, l_q1);
+	gmp_printf("ct      = %Zx\n", l_ct);
+
+	// choose e, so that e is coprime with ct
+	mpz_t l_e;
+	mpz_init(l_e);
+	mpz_set_ui(l_e, 13); // start at 17 after nextprime is called
+	mpz_t l_tmp;
+	mpz_init(l_tmp);
+	do {
+		mpz_nextprime(l_e, l_e);
+		gmp_printf("testing e = %Zd...\n", l_e);
+		mpz_gcd(l_tmp, l_e, l_ct);
+	} while (mpz_cmp_ui(l_tmp, 1) != 0);
+
+	// choose d
+	mpz_t l_d;
+	mpz_init(l_d);
+	if (mpz_invert(l_d, l_e, l_ct) == 0) {
+		fprintf(stderr, "invert failed!\n");
+	} else {
+		gmp_printf("d       = %Zx\n", l_d);
+	}
+
+	mpz_t l_plain;
+	mpz_init(l_plain);
+	mpz_t l_cipher;
+	mpz_init(l_cipher);
+	mpz_t l_decrypted;
+	mpz_init(l_decrypted);
+	for (i = 0; i < 10; ++i) {
+		res = read(l_urandom_fd, l_pt, ((l_bits * 2) / 8) - 1);
+		if (res != ((l_bits * 2) / 8) - 1) {
+			fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		mpz_import(l_plain, ((l_bits * 2) / 8) - 1, 1, sizeof(unsigned char), 0, 0, l_pt);
+		mpz_powm(l_cipher, l_plain, l_e, l_n);
+		mpz_powm(l_decrypted, l_cipher, l_d, l_n);
+		gmp_printf("plain = %Zx cipher = %Zx decrypted = %Zx %d\n", l_plain, l_cipher, l_decrypted, mpz_cmp(l_plain, l_decrypted));
+	}
+
+	// clean up
+	mpz_clear(l_p_import);
+	mpz_clear(l_q_import);
+	mpz_clear(l_p1);
+	mpz_clear(l_q1);
+	mpz_clear(l_n);
+//	mpz_clear(l_phi);
+	mpz_clear(l_ct);
+	mpz_clear(l_e);
+	mpz_clear(l_tmp);
+	mpz_clear(l_d);
+	mpz_clear(l_plain);
+	mpz_clear(l_cipher);
+	mpz_clear(l_decrypted);
 
 	/*
 
