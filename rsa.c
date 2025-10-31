a89c128c24397e9fe29d8157d82a9d34c6761e9c
Stephen Sviatko <ssviatko@gmail.com>
2025-10-30 20:15:49 -0700

    harden against week p/q factors

--- rsa.c
+++ rsa.c
@@ -68,101 +68,143 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-//	printf("Preparing %d-bit p value...\n", l_bits);
-	// prepare random n-bit odd number for DH p factor
-	res = read(l_urandom_fd, l_p, (l_bits / 8));
-	if (res != (l_bits / 8)) {
-		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	l_p[0] |= 0x80; // make it between 2^n - 1 and 2^(n-1)
-	l_p[(l_bits / 8) - 1] |= 0x01; // make it odd
-
 	mpz_t l_p_import;
 	mpz_init(l_p_import);
-	mpz_import(l_p_import, (l_bits / 8), 1, sizeof(unsigned char), 0, 0, l_p);
-//	gmp_printf("p = %Zx\n", l_p_import);
-	int l_pp = mpz_probab_prime_p(l_p_import, 50);
-//	printf("mpz_probab_prime_p returned %d.\n", l_pp);
-	if (l_pp == 0) {
-//		printf("calling mpz_nextprime...\n");
-		mpz_nextprime(l_p_import, l_p_import);
-	}
-	gmp_printf("p       = %Zx\n", l_p_import);
-	l_pp = mpz_probab_prime_p(l_p_import, 50);
-//	printf("mpz_probab_prime_p now returns %d.\n", l_pp);
-
-//	printf("Preparing %d-bit q value...\n", l_bits);
-	// prepare random n-bit odd number for DH p factor
-	res = read(l_urandom_fd, l_q, (l_bits / 8));
-	if (res != (l_bits / 8)) {
-		fprintf(stderr, "rsa: problems reading /dev/urandom: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	l_q[0] |= 0x80; // make it between 2^n - 1 and 2^(n-1)
-	l_q[(l_bits / 8) - 1] |= 0x01; // make it odd
-
 	mpz_t l_q_import;
 	mpz_init(l_q_import);
-	mpz_import(l_q_import, (l_bits / 8), 1, sizeof(unsigned char), 0, 0, l_q);
-//	gmp_printf("q = %Zx\n", l_q_import);
-	l_pp = mpz_probab_prime_p(l_q_import, 50);
-//	printf("mpz_probab_prime_p returned %d.\n", l_pp);
-	if (l_pp == 0) {
-//		printf("calling mpz_nextprime...\n");
-		mpz_nextprime(l_q_import, l_q_import);
-	}
-	gmp_printf("q       = %Zx\n", l_q_import);
-	l_pp = mpz_probab_prime_p(l_q_import, 50);
-//	printf("mpz_probab_prime_p now returns %d.\n", l_pp);
-
-	// establish p-1 and q-1
 	mpz_t l_p1;
 	mpz_init(l_p1);
-	mpz_sub_ui(l_p1, l_p_import, 1);
 	mpz_t l_q1;
 	mpz_init(l_q1);
-	mpz_sub_ui(l_q1, l_q_import, 1);
-	gmp_printf("(p - 1) = %Zx\n", l_p1);
-	gmp_printf("(q - 1) = %Zx\n", l_q1);
-	
-	// prepare n = p * q
 	mpz_t l_n;
 	mpz_init(l_n);
-	mpz_mul(l_n, l_p_import, l_q_import);
-	gmp_printf("n       = %Zx\n", l_n);
-
-	// prepare phi = p1 * q1
-//	mpz_t l_phi;
-//	mpz_init(l_phi);
-//	mpz_mul(l_phi, l_p1, l_q1);
-//	gmp_printf("phi     = %Zx\n", l_phi);
-
-	// prepare carmichael totient
 	mpz_t l_ct;
 	mpz_init(l_ct);
-	mpz_lcm(l_ct, l_p1, l_q1);
-	gmp_printf("ct      = %Zx\n", l_ct);
-
-	// choose e, so that e is coprime with ct
 	mpz_t l_e;
 	mpz_init(l_e);
-	mpz_set_ui(l_e, 13); // start at 17 after nextprime is called
 	mpz_t l_tmp;
 	mpz_init(l_tmp);
-	do {
-		mpz_nextprime(l_e, l_e);
-		gmp_printf("testing e = %Zd...\n", l_e);
-		mpz_gcd(l_tmp, l_e, l_ct);
-	} while (mpz_cmp_ui(l_tmp, 1) != 0);
-
-	// choose d
 	mpz_t l_d;
 	mpz_init(l_d);
-	if (mpz_invert(l_d, l_e, l_ct) == 0) {
-		fprintf(stderr, "invert failed!\n");
-	} else {
-		gmp_printf("d       = %Zx\n", l_d);
+	mpz_t l_q2;
+	mpz_init(l_q2);
+	mpz_t l_counter;
+	mpz_init(l_counter);
+	int l_success = 0;
+	unsigned int l_attempt = 1;
+	while (l_success == 0) {
+		printf("attempt %d to generate key...\n", l_attempt++);
+
+		// prepare random n-bit odd number for p factor
+		res = read(l_urandom_fd, l_p, (l_bits / 8));
+		if (res != (l_bits / 8)) {
+			fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		l_p[0] |= 0x80; // make it between 2^n - 1 and 2^(n-1)
+		l_p[(l_bits / 8) - 1] |= 0x01; // make it odd
+
+		mpz_import(l_p_import, (l_bits / 8), 1, sizeof(unsigned char), 0, 0, l_p);
+		int l_pp = mpz_probab_prime_p(l_p_import, 50);
+		if (l_pp == 0) {
+			mpz_nextprime(l_p_import, l_p_import);
+		}
+		gmp_printf("p       = %Zx\n", l_p_import);
+		l_pp = mpz_probab_prime_p(l_p_import, 50);
+
+		// prepare random n-bit odd number for q factor
+		res = read(l_urandom_fd, l_q, (l_bits / 8));
+		if (res != (l_bits / 8)) {
+			fprintf(stderr, "rsa: problems reading /dev/urandom: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		l_q[0] &= 0x7f; // set up q to hopefully be < p/2
+		l_q[(l_bits / 8) - 1] |= 0x01; // make it odd
+
+		mpz_import(l_q_import, (l_bits / 8), 1, sizeof(unsigned char), 0, 0, l_q);
+		l_pp = mpz_probab_prime_p(l_q_import, 50);
+		if (l_pp == 0) {
+			mpz_nextprime(l_q_import, l_q_import);
+		}
+		gmp_printf("q       = %Zx\n", l_q_import);
+		l_pp = mpz_probab_prime_p(l_q_import, 50);
+
+		// p and q should not be identical
+		if (mpz_cmp(l_p_import, l_q_import) == 0) {
+			fprintf(stderr, "error: p and q cannot be identical.");
+			continue;
+		}
+
+		// p should be > than 2q
+		mpz_mul_ui(l_q2, l_q_import, 2);
+		if (mpz_cmp(l_q2, l_p_import) >= 0) {
+			fprintf(stderr, "error: p must be greater than 2q.\n");
+			continue;
+		}
+
+		// establish p-1 and q-1
+		mpz_sub_ui(l_p1, l_p_import, 1);
+		mpz_sub_ui(l_q1, l_q_import, 1);
+		gmp_printf("(p - 1) = %Zx\n", l_p1);
+		gmp_printf("(q - 1) = %Zx\n", l_q1);
+
+		// p-1 and q-1 should not have small prime factors. Check both of them for all primes <100
+		mpz_set_ui(l_counter, 2); // start with 3 as all even numbers are divisible by 2
+		int l_bailout = 0;
+		do {
+			mpz_nextprime(l_counter, l_counter);
+//			gmp_printf("testing %Zd against P - 1...\n", l_counter);
+			mpz_gcd(l_tmp, l_counter, l_p1);
+			if (mpz_cmp(l_tmp, l_counter) == 0) {
+				l_bailout = 1;
+				break;
+			}
+		} while (mpz_cmp_ui(l_counter, 100) <= 0);
+		if (l_bailout == 1) {
+			gmp_printf("error: (p - 1) value has small prime factor of %Zd.\n", l_counter);
+			continue;
+		}
+
+		mpz_set_ui(l_counter, 2); // start with 3 as all even numbers are divisible by 2
+		l_bailout = 0;
+		do {
+			mpz_nextprime(l_counter, l_counter);
+//			gmp_printf("testing %Zd against Q - 1...\n", l_counter);
+			mpz_gcd(l_tmp, l_counter, l_q1);
+			if (mpz_cmp(l_tmp, l_counter) == 0) {
+				l_bailout = 1;
+				break;
+			}
+		} while (mpz_cmp_ui(l_counter, 100) <= 0);
+		if (l_bailout == 1) {
+			gmp_printf("error: (q - 1) value has small prime factor of %Zd.\n", l_counter);
+			continue;
+		}
+
+		// prepare n = p * q
+		mpz_mul(l_n, l_p_import, l_q_import);
+		gmp_printf("n       = %Zx\n", l_n);
+
+		// prepare carmichael totient
+		mpz_lcm(l_ct, l_p1, l_q1);
+		gmp_printf("ct      = %Zx\n", l_ct);
+
+		// choose e, so that e is coprime with ct
+		mpz_set_ui(l_e, 65536); // start at 65537 after nextprime is called
+		do {
+			mpz_nextprime(l_e, l_e);
+			gmp_printf("testing e = %Zd...\n", l_e);
+			mpz_gcd(l_tmp, l_e, l_ct);
+		} while (mpz_cmp_ui(l_tmp, 1) != 0);
+
+		// choose d
+		if (mpz_invert(l_d, l_e, l_ct) == 0) {
+			fprintf(stderr, "invert failed!\n");
+			continue;
+		} else {
+			gmp_printf("d       = %Zx\n", l_d);
+		}
+		l_success = 1; // made it this far, we generated a key pair!
 	}
 
 	mpz_t l_plain;
@@ -189,7 +231,6 @@ int main(int argc, char **argv)
 	mpz_clear(l_p1);
 	mpz_clear(l_q1);
 	mpz_clear(l_n);
-//	mpz_clear(l_phi);
 	mpz_clear(l_ct);
 	mpz_clear(l_e);
 	mpz_clear(l_tmp);
@@ -197,83 +238,9 @@ int main(int argc, char **argv)
 	mpz_clear(l_plain);
 	mpz_clear(l_cipher);
 	mpz_clear(l_decrypted);
+	mpz_clear(l_q2);
+	mpz_clear(l_counter);
 
-	/*
-
-	printf("Preparing %d-bit a value...\n", l_privbits);
-	res = read(l_urandom_fd, l_a, (l_privbits / 8));
-	if (res != (l_privbits / 8)) {
-		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	mpz_t l_a_import;
-	mpz_init(l_a_import);
-	mpz_import(l_a_import, (l_privbits / 8), 1, sizeof(unsigned char), 0, 0, l_a);
-	gmp_printf("a = %Zx\n", l_a_import);
-
-	printf("Preparing %d-bit b value...\n", l_privbits);
-	res = read(l_urandom_fd, l_b, (l_privbits / 8));
-	if (res != (l_privbits / 8)) {
-		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	mpz_t l_b_import;
-	mpz_init(l_b_import);
-	mpz_import(l_b_import, (l_privbits / 8), 1, sizeof(unsigned char), 0, 0, l_b);
-	gmp_printf("b = %Zx\n", l_b_import);
-
-	printf("Preparing g value...\n");
-	mpz_t l_g;
-	mpz_init(l_g);
-	unsigned int l_g_rand;
-	res = read(l_urandom_fd, &l_g_rand, sizeof(l_g_rand));
-	if (res != sizeof(l_g_rand)) {
-		fprintf(stderr, "dh: problems reading /dev/urandom: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	// l_g_rand even/odd?
-	if ((l_g_rand & 0x01) == 0)
-		mpz_set_ui(l_g, 3);
-	else
-		mpz_set_ui(l_g, 5);
-	gmp_printf("g = %Zd\n", l_g);
-
-	// done with random values, so close urandom_fd
-	close(l_urandom_fd);
-
-	/* at this point in the DH exchange, Alice would have already generated p and g
-	 * (and send them to Bob,)
-	 * so that both Alice and Bob have the same p and g. Then each Alice and Bob would
-	 * generate a private value (a for Alice, b for Bob) and keep these secret. Then
-	 * Alice computes A = g^a mod p, Bob computes B = g^b mod p, and Alice and Bob
-	 * exchange A and B eith each other while keeping a and b secret. */
-/*
-	mpz_t l_A;
-	mpz_init(l_A);
-	mpz_powm(l_A, l_g, l_a_import, l_p_import);
-	gmp_printf("A = %Zx\n", l_A);
-
-	mpz_t l_B;
-	mpz_init(l_B);
-	mpz_powm(l_B, l_g, l_b_import, l_p_import);
-	gmp_printf("B = %Zx\n", l_B);
-
-	/* Now Alice and Bob have each other's modexp computed values A and B, so Alice
-	 * takes Bob's B and computes sa = B^a mod p. Bob takes Alice's A and computes
-	 * sb = A^b mod p. When they are finished, sa and sb should match on either
-	 * side of the transaction, and this value can be used to create a symmetric
-	 * encryption key. */
-/*
-	mpz_t l_sa;
-	mpz_init(l_sa);
-	mpz_powm(l_sa, l_B, l_a_import, l_p_import);
-	gmp_printf("sa = %Zx\n", l_sa);
-
-	mpz_t l_sb;
-	mpz_init(l_sb);
-	mpz_powm(l_sb, l_A, l_b_import, l_p_import);
-	gmp_printf("sb = %Zx\n", l_sb);
-*/
 	return 0;
 }
 
